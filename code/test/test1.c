483cb84f4e8c6584b0d88aa8c2d7e8af85e578ce
weipooppys93030 <weipooppys93030@gmail.com>
2019-12-16 15:27:16 +0800

    init

---
+++ code/test/test1.c
@@ -0,0 +1,9 @@
+#include "syscall.h"
+main()
+{
+
+	int n;
+
+	for (n = 5; n < 9; n++)
+		PrintInt(n);
+}
---
+++ code/test/test3.c
@@ -0,0 +1,7 @@
+#include "syscall.h"
+main()
+	{
+		int	n;
+		for (n=100;n>90;n--)
+			PrintInt(n);
+	}
