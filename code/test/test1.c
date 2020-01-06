3d43f88d936baf9dc81a090044bd3c6d454b4f8d
weipooppys93030 <weipooppys93030@gmail.com>
2020-01-06 18:47:28 +0800

    test

--- code/test/test1.c
+++
@@ -1,9 +0,0 @@
-#include "syscall.h"
-main()
-{
-
-	int n;
-
-	for (n = 5; n < 9; n++)
-		PrintInt(n);
-}
--- code/test/test3.c
+++
@@ -1,7 +0,0 @@
-#include "syscall.h"
-main()
-	{
-		int	n;
-		for (n=100;n>90;n--)
-			PrintInt(n);
-	}
