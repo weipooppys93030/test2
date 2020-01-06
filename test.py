9752229d62fa3fbb24954fd4d54ff5844bb15bf9
weipooppys93030 <weipooppys93030@gmail.com>
2020-01-06 23:58:42 +0800

    Update test.py

--- test.py
+++ test.py
@@ -20,3 +20,17 @@ one = pd.read_csv(
 zero = pd.read_csv(
     './zero.csv')
 
+print('chinese', df[df.language == 3].shape[0],
+      one[one.language == 3].shape[0], zero[zero.language == 3].shape[0])
+
+print('english', df[df.language == 52].shape[0],
+      one[one.language == 52].shape[0], zero[zero.language == 52].shape[0])
+
+print('korean', df[df.language == 31].shape[0],
+      one[one.language == 31].shape[0], zero[zero.language == 31].shape[0])
+
+print('japanese', df[df.langage == 17].shape[0],
+      one[one.language == 17].shape[0], zero[zero.language == 17].shape[0])
+
+print('taiwanese', df[df.langage == 10].shape[0],
+      one[one.language == 10].shape[0], zero[zero.language == 10].shape[0])
