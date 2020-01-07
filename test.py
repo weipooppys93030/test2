2332eaec626a45dcb4c284d43aaba3c9f9264221
weipooppys93030 <weipooppys93030@gmail.com>
2020-01-07 12:20:57 +0800

    t

--- test.py
+++ test.py
@@ -10,12 +10,42 @@ from sklearn import linear_model
 from sklearn.metrics import accuracy_score
 import csv
 
-
 train = pd.read_csv(
-    './merge2.csv')
+    './test.csv')
+songs = pd.read_csv(
+    './songs.csv')
 
-print(train.shape)
+# songs.pop('genre_ids')
+songs.pop('artist_name')
+songs.pop('composer')
+songs.pop('lyricist')
 
-ans = train.pop('target')
-train = train.join(ans)
-print(train.shape)
+# members = pd.read_csv(
+#    '/Users/jimmy/Desktop/Python/project/kkbox-music-recommendation-challenge/members.csv')
+
+members = pd.read_csv(
+    './members.csv')
+
+
+# members.pop('gender')
+
+expiration = members.pop('expiration_date')
+last = expiration - members['registration_init_time']
+expiration = pd.concat([expiration, last], axis=1)
+expiration.columns = ['expiration_date', 'remaining_time']
+members = pd.concat([members, expiration], axis=1)
+
+
+songs.set_index('song_id', inplace=True)
+members.set_index('msno', inplace=True)
+
+train.set_index('song_id', inplace=True)
+train = train.join(songs)
+
+train.set_index('msno', inplace=True)
+train = train.join(members)
+
+train.fillna(value=0, inplace=True)
+
+train.to_csv(
+    './test2.csv', index=False)
